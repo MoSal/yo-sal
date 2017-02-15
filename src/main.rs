e3212f7067a3df4d2fb82aad752bb6c456050f00
Mohammad AlSaleh <CE.Mohammad.AlSaleh@gmail.com>
2017-02-15 09:59:51 +0200

    More resilient
    Signed-off-by: Mohammad AlSaleh <CE.Mohammad.AlSaleh@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -22,30 +22,30 @@ fn main() {
     println!("Parsing JSON...");
     let yt_json = json::parse(&String::from_utf8_lossy(&cmd.stdout)).unwrap();
     let fname = format!("{}-{}", yt_json["fulltitle"], yt_json["id"]);
-    if let JsonValue::Array(ref formats) = yt_json["formats"] {
-        let info = formats.iter()
-            .filter(|fmt| fmt["protocol"].as_str().unwrap().starts_with("http"))
+    let info = if let JsonValue::Array(ref formats) = yt_json["formats"] {
+        formats.iter()
+            .filter(|fmt| fmt["protocol"].as_str().unwrap().starts_with("http") || fmt["protocol"].as_str().unwrap() == "")
             .last()
-            .unwrap();
-
-        let mut args = vec!["-nTrm".into(), "-a2".into()];
-        args.push(format!("{}", info["url"]));
-        args.push("-o".into());
-        args.push(format!("{}.{}", fname, info["ext"]).replace(" ", "_"));
-
-        if let JsonValue::Object(ref hdrs_json) = info["http_headers"] {
-            for (k, v) in hdrs_json.iter() {
-                args.push("--custom-headers".into());
-                args.push(format!("{}: {}", k, v));
-            }
-        }
-        println!("Running saldl with args: {:?}\n==========\n", args);
-        let _ = Command::new("saldl")
-            .args(&args)
-            .status()
-            .unwrap();
+            .unwrap()
     } else {
-        panic!("No formats array.");
-    }
+        // generic, no formats
+        &yt_json
+    };
 
+    let mut args = vec!["-nTrm".into(), "-a2".into()];
+    args.push(format!("{}", info["url"]).trim_matches('"').to_string());
+    args.push("-o".into());
+    args.push(format!("{}.{}", fname, info["ext"]).replace(" ", "_"));
+
+    if let JsonValue::Object(ref hdrs_json) = info["http_headers"] {
+        for (k, v) in hdrs_json.iter() {
+            args.push("-H".into());
+            args.push(format!("{}: {}", k, v));
+        }
+    }
+    println!("Running saldl with args: {:?}\n==========\n", args);
+    let _ = Command::new("saldl")
+        .args(&args)
+        .status()
+        .unwrap();
 }
